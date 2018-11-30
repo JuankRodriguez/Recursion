49d55428219dd0ba6c478360340e50e5d2e97070
UribesEffect <38766156+UribesEffect@users.noreply.github.com>
2018-11-30 08:37:56 -0500

    Update sumasyrestas_recur.py

--- sumasyrestas_recur.py
+++ sumasyrestas_recur.py
@@ -10,8 +10,8 @@ def operar(numero1, numero2):
         return operar(numero1+1, numero2-1) #recursividad
  
  
-print("10+3=", operar(10, 3))
-print("5-2=", operar(5, -2))
+print("10+3=", operar(5, 2))
+print("5-2=", operar(6, -2))
 
 # este ejemplo es muy comprensible pero ineficiente ya que la funcion operar 
 # se realiza varias veces gastando memoria y tiempo 
